6755130ba304b51d5825c7901a8e2201e6576f38
azimmalik9 <azim.malik9@gmail.com>
2017-05-30 17:07:53 +0100

    Centos vagrant and bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,52 +1,50 @@
 #!/usr/bin/env bash
 
 ifconfig
-sudo apt-get update -y
+sudo yum update -y
 
-cd /
-cd /opt
-
+cd /vagrant/
+scp java.tar.gz /opt/
+scp maven.tar.gz /opt/
+scp jenkins_2.1_all.deb /opt/
+cd /opt/
 sudo tar zxvf java.tar.gz
 
 sudo update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_45/bin/java 100
 sudo update-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_45/bin/javac 100
-sudo update-alternatives --config java 
-java -version
+#sudo update-alternatives --config java 
+
 
 sudo tar zxvf maven.tar.gz
 sudo update-alternatives --install /usr/bin/mvn mvn /opt/apache-maven-3.3.9/bin/mvn 100
-mvn -version
+
 
 #install Git
-sudo apt-get install -y git
+sudo yum install -y git
 java -version
 mvn -v
 git --version
-#git -version 
+#git -version
 
 cd /opt                
-                   
-
-sudo dpkg -i jenkins_2.1_all.deb
-sudo apt-get install -f -y  
-  
-sudo apt-get install jenkins
-
-sudo ssh-keygen
-sudo service jenkins start  
                        
-   
+dpkg -i jenkins_2.1_all.deb
+sudo yum install -f -y  
+sudo yum install -y jenkins
 
+service jenkins start  
                        
-chmod a+x jira.bin     
-sudo ./jira.bin << EOF
-o
-1
-2
-8081
-8006
-i
-EOF
-
-sudo service jenkins status 
+jenkins -version      
+sudo service jenkins status
+#                       
+#chmod a+x jira.bin     
+# sudo ./jira.bin << EOF
+#o                      
+#1
+#2
+#8081
+#8006
+#i
+#EOF
+# 
 echo "Finished installing"
